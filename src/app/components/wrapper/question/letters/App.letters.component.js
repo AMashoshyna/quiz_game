36b162bd1eafa3f9d8c7a565624dfe9b5f2ffb71
AMashoshyna <a.mashoshyna@gmail.com>
2016-12-12 00:32:29 +0200

    implement letter shuffling

--- src/app/components/wrapper/question/letters/App.letters.component.js
+++ src/app/components/wrapper/question/letters/App.letters.component.js
@@ -2,6 +2,14 @@
 	'use strict';
 	angular.module('App')
 	.component('letters', {
-		
-	})
+		templateUrl: 'src/app/components/wrapper/question/letters/letters_template.html',
+		controller: lettersController
+	});
+
+	lettersController.$inject = ['QuizService']
+	function lettersController(QuizService) {
+		var ctrl = this;
+
+		ctrl.answer = QuizService.currentQuestion;
+	}
 })();
--- src/app/data/QuizService.js
+++ src/app/data/QuizService.js
@@ -4,7 +4,7 @@
 	.service('QuizService', QuizService);
 
 
-QuizService.$inject = ['$http', 'apiPath'];
+	QuizService.$inject = ['$http', 'apiPath'];
 	function QuizService($http, apiPath) {
 		var service = this;
 		service.currentQuestion = {};
@@ -16,7 +16,6 @@ QuizService.$inject = ['$http', 'apiPath'];
 
 		service.getNewQuestion();
 
-
 		function checkAnswer(answer) {
 			if(answer.toLowerCase()!== service.currentQuestion.answer) {
 				service.acceptAnswer = false;
@@ -27,8 +26,22 @@ QuizService.$inject = ['$http', 'apiPath'];
 			return $http.get(apiPath)
 			.then((response) => {
 				service.currentQuestion.detail = response.data[0];
-				console.log(service.currentQuestion);
+				service.currentQuestion.shuffledAnswer = shuffleAnswer(service.currentQuestion.detail.answer);
 			})
 		};
+
+		function shuffleAnswer(answer) {
+			answer = answer.toLowerCase();
+			var arr = answer.split('');
+			return shuffle(arr);
+		}
+
+		function shuffle(a) {
+			for (let i = a.length; i; i--) {
+				let j = Math.floor(Math.random() * i);
+				[a[i - 1], a[j]] = [a[j], a[i - 1]];
+			}
+				return a;
+		}
 	}
 })();
